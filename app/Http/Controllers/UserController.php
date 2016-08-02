cafbf10d148337c6b903847e53c033c351dfacde
Abhimanyu310 <abhip310@gmail.com>
2016-08-02 18:23:38 +0530

    Initial Commit. Created products page, products model, routes for products and users, headers and basic stuff

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Auth;
+
+class UserController extends Controller
+{
+    public function getSignup()
+    {
+
+    }
+
+    public function postSignup(Request $request)
+    {
+
+    }
+    public function getSignin()
+    {
+
+    }
+
+    public function postSignin(Request $request)
+    {
+        //
+    }
+
+    public function getProfile() {
+
+    }
+
+    public function getLogout() {
+
+    }
+}
